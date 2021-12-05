210b98fe514ee9891ef6ff5f7ac66bec553a701c
dpak1999 <deepakkumardash313@gmail.com>
2021-12-05 20:48:27 +0530

    check is square

--- src/components/IsSquare.js
+++ src/components/IsSquare.js
@@ -3,18 +3,20 @@
 import { useState } from 'react';
 
 const IsSquare = () => {
+  const [message, setMessage] = useState('');
+  const [flag, setFlag] = useState(false);
   const [side, setSide] = useState({
-    firstSide: '',
-    secondSide: '',
-    thirdSide: '',
-    fourthSide: '',
+    firstSide: '20',
+    secondSide: '30',
+    thirdSide: '40',
+    fourthSide: '50',
   });
 
   const [angle, setAngle] = useState({
-    firstAngle: '',
-    secondAngle: '',
-    thirdAngle: '',
-    fourthAngle: '',
+    firstAngle: '90',
+    secondAngle: '90',
+    thirdAngle: '90',
+    fourthAngle: '90',
   });
 
   const handleSideChange = (e) => {
@@ -33,43 +35,63 @@ const IsSquare = () => {
     });
   };
 
+  const handleClick = () => {
+    const sides = Object.values(side);
+    const angles = Object.values(angle);
+
+    const allSides = sides.every((value) => value === sides[0]);
+    const allAngles = angles.every((value) => value === angles[0]);
+
+    if (allSides && allAngles && angle.firstAngle === '90') {
+      setMessage('Yayy it is a square');
+      setFlag(true);
+    } else {
+      setMessage('Uh! Oh it is not a square');
+      setFlag(false);
+    }
+  };
+
   return (
     <div className="container mx-auto px-4">
       <div className="flex items-center justify-around py-4">
         <div>
           <h1 className="text-xl font-mono">Enter first side</h1>
           <input
+            required
             value={side.firstSide}
             name="firstSide"
             onChange={handleSideChange}
-            type="text"
+            type="number"
             className="border border-purple-400 w-80 p-2 rounded outline-none mb-5"
           />
 
           <h1 className="text-xl font-mono">Enter second side</h1>
           <input
+            required
             value={side.secondSide}
             name="secondSide"
             onChange={handleSideChange}
-            type="text"
+            type="number"
             className="border border-purple-400 w-80 p-2 rounded outline-none mb-5"
           />
 
           <h1 className="text-xl font-mono">Enter third side</h1>
           <input
+            required
             value={side.thirdSide}
             name="thirdSide"
             onChange={handleSideChange}
-            type="text"
+            type="number"
             className="border border-purple-400 w-80 p-2 rounded outline-none mb-5"
           />
 
           <h1 className="text-xl font-mono">Enter fourth side</h1>
           <input
+            required
             value={side.fourthSide}
             name="fourthSide"
             onChange={handleSideChange}
-            type="text"
+            type="number"
             className="border border-purple-400 w-80 p-2 rounded outline-none mb-5"
           />
         </div>
@@ -80,7 +102,7 @@ const IsSquare = () => {
             value={angle.firstAngle}
             name="firstAngle"
             onChange={handleAngleChange}
-            type="text"
+            type="number"
             className="border border-purple-400 w-80 p-2 rounded outline-none mb-5"
           />
 
@@ -89,7 +111,7 @@ const IsSquare = () => {
             value={angle.secondAngle}
             name="secondAngle"
             onChange={handleAngleChange}
-            type="text"
+            type="number"
             className="border border-purple-400 w-80 p-2 rounded outline-none mb-5"
           />
 
@@ -98,7 +120,7 @@ const IsSquare = () => {
             value={angle.thirdAngle}
             name="thirdAngle"
             onChange={handleAngleChange}
-            type="text"
+            type="number"
             className="border border-purple-400 w-80 p-2 rounded outline-none mb-5"
           />
 
@@ -107,10 +129,27 @@ const IsSquare = () => {
             value={angle.fourthAngle}
             name="fourthAngle"
             onChange={handleAngleChange}
-            type="text"
+            type="number"
             className="border border-purple-400 w-80 p-2 rounded outline-none mb-5"
           />
         </div>
+      </div>
+
+      <div
+        className={`text-center mb-6 text-2xl font-mono ${
+          flag ? `text-green-500` : `text-red-500`
+        }`}
+      >
+        {message && message}
+      </div>
+
+      <div className="text-center">
+        <button
+          onClick={handleClick}
+          className="border border-purple-500 rounded py-2 px-5 cursor-pointer hover:bg-purple-500 hover:text-white"
+        >
+          Check
+        </button>
       </div>
     </div>
   );
